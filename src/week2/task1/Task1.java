92214815c564fe127939599dfa0dde95504d5618
traidat <43227093+traidat@users.noreply.github.com>
2018-09-27 09:37:47 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -13,8 +13,10 @@ public class Task1 {
     }
 
     public static int fibonacci(int n) {
-        if (n <= 1)
+        if (n == 1)
             return 1;
+        else if (n == 0)
+            return 0;
         else return fibonacci(n - 1) + fibonacci(n - 2);
     }
     public static void main(String[] args) {
