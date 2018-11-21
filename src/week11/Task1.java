e9f0d3b17121d45dff76901e2cb31dde4c1716ed
traidat <truong5a8@gmail.com>
2018-11-21 21:05:20 +0700

    BT week 2

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,36 @@
 package week11;
 
 public class Task1 {
+    static <T extends Comparable<T>> void  sort(T[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i].compareTo(arr[j]) > 0) {
+                    T t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Integer[] i = {2,5,1,6,7};
+        Double[] d = {3.4,5.6,7.8,9.0};
+        Character[] c = {'a','t','b','A'};
+        Long[] l = {-1l,99999l,898l,4l};
+        Float[] f = {-1.3f, 6.7f, -999.6f,0f};
+        Short[] s = {-8,6,2,3,9};
+        Byte[] b = {6,9,7,0};
+        sort(i);
+        sort(d);
+        sort(c);
+        sort(l);
+        sort(f);
+        sort(s);
+        sort(b);
+
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,61 @@
 package week11;
 
+import javax.naming.InsufficientResourcesException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task2 {
+    static <T extends Comparable<T>> void  maxOfArrayList(List <T> arr){
+        T max = arr.get(0);
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr.get(i).compareTo(max) > 0) {
+                max = arr.get(i);
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static void main(String[] args) {
+        List<Character> c = new ArrayList<Character>();
+        List<Integer> i = new ArrayList<Integer>();
+        List<Double> d = new ArrayList<Double>();
+        List<Float> f = new ArrayList<Float>();
+        List<Short> s = new ArrayList<Short>();
+        List<Byte> b = new ArrayList<Byte>();
+        List<Long> l = new ArrayList<Long>();
+        c.add('h');
+        c.add('v');
+        c.add('A');
+        i.add(1);
+        i.add(19);
+        i.add(100);
+        i.add(11);
+        d.add(-8.5);
+        d.add(-5.0);
+        d.add(-80.5);
+        d.add(8.5);
+        f.add(-8.5f);
+        f.add(-5.0f);
+        f.add(-80.5f);
+        f.add(8.5f);
+        s.add((short) 0);
+        s.add((short) 19);
+        s.add((short) 100);
+        s.add((short) 11);
+        b.add((byte) 33);
+        b.add((byte) 3);
+        b.add((byte) -33);
+        b.add((byte) 0);
+        l.add(1l);
+        l.add(-1l);
+        l.add(13l);
+        l.add(-13l);
+        maxOfArrayList(i);
+        maxOfArrayList(d);
+        maxOfArrayList(s);
+        maxOfArrayList(l);
+        maxOfArrayList(b);
+        maxOfArrayList(c);
+        maxOfArrayList(f);
+    }
 }
